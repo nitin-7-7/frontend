370bd41d424d160b55692fa7f062dc862b7c5474
nitin-7-7 <nitincatt@gmail.com>
2025-11-18 23:09:23 +0530

    tsk

--- HTML/Experiment/experiment-05/index.js
+++ HTML/Experiment/experiment-05/index.js
@@ -7,9 +7,33 @@
 
 // task 2:
 
-const age = prompt("AGE BTAA !")
+/*
+const age = prompt("AGE BTAAO !")
 if(age>18){
     alert("votes deshkta hai bhai tu ")
-}else{
+}else if(age>60){
+    alert("You are a senior citizen voter.")
+}
+else{
     alert("jake POGO dekh!")
 }
+*/
+// task 3 
+
+let arr = [2,3,4,5,6,7,8,8,9,23,5,65657];
+
+//use of map
+
+let triple = arr.map(n => n*3);
+console.log(triple);
+
+//use filter (if)
+let num30 = arr.filter(n => n>30);
+console.log(num30);
+
+//reduce work as sum of arr
+let sum = arr.reduce((total,num) => total+num,0);
+console.log(sum);
+
+//use of foreach
+arr.forEach(num => console.log(num*2));
